3dfe58862a759bb182fd0dd1bfa474cfa909b3d3
Sudeep Kumar <kkumar.sudeep@gmail.com>
2016-07-20 22:01:23 +1000

    txn-mgmt

--- src/main/java/org/example/ws/service/GreetingServiceBean.java
+++ src/main/java/org/example/ws/service/GreetingServiceBean.java
@@ -4,12 +4,15 @@ import org.example.ws.model.Greeting;
 import org.example.ws.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 
     @Autowired
@@ -28,6 +31,7 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public Greeting create(Greeting greeting) {
         if(greeting.getId() != null) {
             //Cannot create Greeting with specified ID value
@@ -35,10 +39,17 @@ public class GreetingServiceBean implements GreetingService {
         }
 
         Greeting savedGreeting = greetingRepository.save(greeting);
+
+        //Illustrate Txn rollback
+        if(savedGreeting.getId() == 4L) {
+            throw new RuntimeException("Roll me back");
+        }
+
         return savedGreeting;
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public Greeting update(Greeting greeting) {
         Greeting persistedGreeting = findOne(greeting.getId());
 
@@ -52,6 +63,7 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public void delete(Long id) {
         greetingRepository.delete(id);
     }
