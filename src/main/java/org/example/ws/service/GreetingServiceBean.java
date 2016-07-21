eddb026d4ce2a8e2283290e123395883243e7e43
Sudeep Kumar <kkumar.sudeep@gmail.com>
2016-07-21 20:09:57 +1000

    unit-test

--- src/main/java/org/example/ws/service/GreetingServiceBean.java
+++ src/main/java/org/example/ws/service/GreetingServiceBean.java
@@ -2,6 +2,8 @@ package org.example.ws.service;
 
 import org.example.ws.model.Greeting;
 import org.example.ws.repository.GreetingRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.NoResultException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +21,8 @@ import java.util.Map;
 @Service
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceBean implements GreetingService {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private GreetingRepository greetingRepository;
@@ -38,18 +44,22 @@ public class GreetingServiceBean implements GreetingService {
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     @CachePut(value = "greetings", key = "#result.id")
     public Greeting create(Greeting greeting) {
-        if(greeting.getId() != null) {
-            //Cannot create Greeting with specified ID value
-            return null;
+        logger.info("> create");
+
+        // Ensure the entity object to be created does NOT exist in the
+        // repository. Prevent the default behavior of save() which will update
+        // an existing entity if the entity matching the supplied id exists.
+        if (greeting.getId() != null) {
+            // Cannot create Greeting with specified ID value
+            logger.error(
+                    "Attempted to create a Greeting, but id attribute was not null.");
+            throw new EntityExistsException(
+                    "The id attribute must be null to persist a new entity.");
         }
 
         Greeting savedGreeting = greetingRepository.save(greeting);
 
-        //Illustrate Txn rollback
-        if(savedGreeting.getId() == 4L) {
-            throw new RuntimeException("Roll me back");
-        }
-
+        logger.info("< create");
         return savedGreeting;
     }
 
@@ -57,16 +67,26 @@ public class GreetingServiceBean implements GreetingService {
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     @CachePut(value = "greetings", key = "#greeting.id")
     public Greeting update(Greeting greeting) {
-        Greeting persistedGreeting = findOne(greeting.getId());
+        logger.info("> update id:{}", greeting.getId());
 
-        if(persistedGreeting == null) {
-            //Cannot update Greeting that hasn't been persisted
-            return null;
+        // Ensure the entity object to be updated exists in the repository to
+        // prevent the default behavior of save() which will persist a new
+        // entity if the entity matching the id does not exist
+        Greeting greetingToUpdate = findOne(greeting.getId());
+        if (greetingToUpdate == null) {
+            // Cannot update Greeting that hasn't been persisted
+            logger.error(
+                    "Attempted to update a Greeting, but the entity does not exist.");
+            throw new NoResultException("Requested entity not found.");
         }
 
-        Greeting updatedGreeting = greetingRepository.save(greeting);
+        greetingToUpdate.setText(greeting.getText());
+        Greeting updatedGreeting = greetingRepository.save(greetingToUpdate);
+
+        logger.info("< update id:{}", greeting.getId());
         return updatedGreeting;
     }
+
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
